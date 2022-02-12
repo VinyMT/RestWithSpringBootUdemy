8c13b4bf8c0f79775adcd790329d611affab5478
VinyMT <viniciusmatiolatramontin@gmail.com>
2022-02-12 10:12:40 -0300

    Project created

---
+++ rest-with-springboot-udemy/src/main/java/com/vinymt/course/RestWithSpringbootUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.vinymt.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringbootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringbootUdemyApplication.class, args);
+	}
+
+}
